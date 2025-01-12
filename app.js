7ff995e8f8c320497e0cd948a71bf39853dde800
ChetanSrikant <chetansmandiga@gmail.com>
2025-01-12 12:11:59 +0530

    ejs

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  //   res.send('Hello World');
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-    res.send('About Page');
+app.get("/about", (req, res) => {
+  res.send("About Page");
 });
 
-app.get('/profile', (req, res) => {
-    res.send('Profile Page');
+app.get("/profile", (req, res) => {
+  res.send("Profile Page");
 });
 
 app.listen(3000);
